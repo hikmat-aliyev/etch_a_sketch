7f8ce17cb02139ab2837c3666c065c6a28500d36
elchinbaba <babalielchin@gmail.com>
2022-12-15 21:26:07 +0400

    edited script.js file for handling input values

--- script.js
+++ script.js
@@ -4,46 +4,70 @@ console.log(squareSide);
 
 let div;
 let squareNumber = 16;
+let squares; // const -> let
 //create grid
 
-for(let i = 0; i < squareNumber * squareNumber; i ++){
-div = document.createElement("div");
-container.appendChild(div);
-div.className = "square";
-div.style.width = `${squareSide / squareNumber}px`;
-div.style.height = div.style.width;
-console.log(`${div.style.height} and ${div.style.width}`)
-}
+function createGrid() {
+    // I converted next lines into function for recalling it later.
 
-const squares = document.querySelectorAll(".square");
+    for (let i = 0; i < squareNumber * squareNumber; i++) {
+        div = document.createElement("div");
+        container.appendChild(div);
+        div.className = "square";
+        div.style.width = `${squareSide / squareNumber}px`;
+        div.style.height = div.style.width;
+        console.log(`${div.style.height} and ${div.style.width}`)
+    }
 
-squares.forEach(square => {
-square.addEventListener("mouseover", event => {
-if(event.buttons === 1) {
-square.classList.add("hovered-square");
-}
-})
-})
+    squares = document.querySelectorAll(".square");
 
-//in mouseover case, you can't color 1 square, that is why I wrote that function with mousedown event
-squares.forEach(square => {
-square.addEventListener("mousedown", event => {
-if(event.buttons === 1) {
-square.classList.add("hovered-square");
+    squares.forEach(square => {
+        square.addEventListener("mouseover", event => {
+            if (event.buttons === 1) {
+                square.classList.add("hovered-square");
+            }
+        });
+    });
+
+    //in mouseover case, you can't color 1 square, that is why I wrote that function with mousedown event
+    squares.forEach(square => {
+        square.addEventListener("mousedown", event => {
+            if (event.buttons === 1) {
+                square.classList.add("hovered-square");
+            }
+        });
+    });
 }
-})
-})
+createGrid();
 
 //we can change width, height of square but can't change i < square * square, it remains as 16 * 16
-function changeBoxSize(){
+function changeBoxSize() {
+    // You need to clean the container and recreate desired squares
 
-squares.forEach(square =>{
-squareNumber = document.getElementById("myInput").value;
-for(let i = 0; i < squareNumber *squareNumber; i++) {
+    const cleanContainer = () => {
+        document.querySelector(".container").innerHTML = "";
+    }
 
-square.style.width = `${squareSide / squareNumber}px`;
-square.style.height = square.style.width; 
+    const recreateSquares = () => {
+        const refreshSquares = () => {
+            squares = document.querySelectorAll(".square");
+        }
 
-}
-})
+        squareNumber = document.getElementById("myInput").value;
+
+        createGrid();
+
+        refreshSquares();
+
+        squares.forEach(square => {
+            for (let i = 0; i < squareNumber * squareNumber; i++) {
+                square.style.width = `${squareSide / squareNumber}px`;
+                square.style.height = square.style.width;
+            }
+        });
+    }
+
+    cleanContainer();
+
+    recreateSquares();
 }
