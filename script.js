d32eb6843c3d24ea6009d65ea0fa99fdde0c8f9d
Hikmat Ali <hikmat.aliyevm@gmail.com>
2022-12-16 22:09:36 +0400

    Edit html, css, js files

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let squareSide = container.offsetWidth - 10;
 console.log(squareSide);
 
 let div;
-let squareNumber = 16;
 let squares; // const -> let
-//create grid
+let squareNumber = 16;
 
-function createGrid() {
+//create grid
+function createGrid(squareNumber) {
     // I converted next lines into function for recalling it later.
 
     for (let i = 0; i < squareNumber * squareNumber; i++) {
@@ -20,11 +20,11 @@ function createGrid() {
     }
 
     squares = document.querySelectorAll(".square");
-
+    let color = document.getElementById("color-input").value;
     squares.forEach(square => {
         square.addEventListener("mouseover", event => {
             if (event.buttons === 1) {
-                square.classList.add("hovered-square");
+                square.style.backgroundColor = color;
             }
         });
     });
@@ -33,12 +33,12 @@ function createGrid() {
     squares.forEach(square => {
         square.addEventListener("mousedown", event => {
             if (event.buttons === 1) {
-                square.classList.add("hovered-square");
+                square.style.backgroundColor = color;
             }
         });
     });
 }
-createGrid();
+createGrid(squareNumber );
 
 //we can change width, height of square but can't change i < square * square, it remains as 16 * 16
 function changeBoxSize() {
@@ -48,26 +48,97 @@ function changeBoxSize() {
         document.querySelector(".container").innerHTML = "";
     }
 
-    const recreateSquares = () => {
-        const refreshSquares = () => {
-            squares = document.querySelectorAll(".square");
-        }
+    squareNumber = document.getElementById("myInput").value;
+    //limit square value to 100
+    if(squareNumber > 100){
+        document.querySelector("p").textContent = "Value should be equal or less than 100"; 
+        return;
+    }
+    else if(squareNumber <= 100){
+        document.querySelector("p").textContent = ""; 
+    }
+    cleanContainer();
+    createGrid(squareNumber);   
+}
 
-        squareNumber = document.getElementById("myInput").value;
+function cleanColor() {
+    document.querySelector(".container").innerHTML = "";
+    //checking if there is input value, else we assign default value which is 16 
+    if(document.getElementById("myInput").value){
+        squareNumber = document.getElementById("myInput").value; 
+    }
+    createGrid(squareNumber);   
+}
 
-        createGrid();
+function changeColor() {
+    
+    let color = document.getElementById("color-input").value;
 
-        refreshSquares();
+    let squares = document.querySelectorAll(".square"); 
 
-        squares.forEach(square => {
-            for (let i = 0; i < squareNumber * squareNumber; i++) {
-                square.style.width = `${squareSide / squareNumber}px`;
-                square.style.height = square.style.width;
+    squares.forEach(square => {
+        square.addEventListener("mouseover", event => {
+            if (event.buttons === 1) {
+                square.style.backgroundColor = color;
             }
         });
+    });
+
+    squares.forEach(square => {
+        square.addEventListener("mousedown", event => {
+            if (event.buttons === 1) {
+                square.style.backgroundColor = color;
+            }
+        });
+    });
+}
+
+
+function rainbow() {
+
+    squares = document.querySelectorAll(".square");
+
+    function randomColor() {
+        let x = Math.floor(Math.random() * 256);
+        let y = Math.floor(Math.random() * 256);
+        let z = Math.floor(Math.random() * 256);
+        let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+        return bgColor;
     }
 
-    cleanContainer();
+    squares.forEach(square => {
+        square.addEventListener("mouseover", event => {
+            if (event.buttons === 1) {
+                square.style.backgroundColor = randomColor();
+            }
+        });
+    });
 
-    recreateSquares();
+    squares.forEach(square => {
+        square.addEventListener("mousedown", event => {
+            if (event.buttons === 1) {
+                square.style.backgroundColor = randomColor();
+            }
+        });
+    });
+
+}
+
+function eraser() {
+    squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener("mouseover", event => {
+            if (event.buttons === 1) {
+                square.style.backgroundColor = "transparent";
+            }
+        });
+    });
+
+    squares.forEach(square => {
+        square.addEventListener("mousedown", event => {
+            if (event.buttons === 1) {
+                square.style.backgroundColor = "transparent";
+            }
+        });
+    });
 }
